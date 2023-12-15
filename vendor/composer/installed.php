b80332effe502c07cffdc0cc25c72814f9b6c0a6
Hamidreza Mozhdeh <hamidreza87m@gmail.com>
2023-12-15 15:41:06 +0330

    First release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2f8b7dbb48cf2fcd66ed3639a1eafde5ca6e6618',
+        'name' => 'hamidreza-mozhdeh/filter-by-query-string',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hamidreza-mozhdeh/filter-by-query-string' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2f8b7dbb48cf2fcd66ed3639a1eafde5ca6e6618',
+            'dev_requirement' => false,
+        ),
+    ),
+);
